9b9c4b62f6a2353ab424eaab11f3d64376163dfa
Yoshioka Tsuneo <yoshiokatsuneo@gmail.com>
2015-07-13 18:01:41 +0900

    Added 'ngMessages' to angularjs app module dependency

--- client/app/app.js
+++ client/app/app.js
@@ -8,7 +8,8 @@ angular.module('paizaqaApp', [
   'ui.router',
   'ui.bootstrap',
   'ui.pagedown',
-  'ngTagsInput'
+  'ngTagsInput',
+  'ngMessages',
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
