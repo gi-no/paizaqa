ccfdf0db071152cc57200b65ae96cfd57caa9084
Yoshioka Tsuneo <yoshiokatsuneo@gmail.com>
2015-07-17 17:28:22 +0900

    Updated style for star

--- client/app/questionsIndex/questionsIndex.controller.js
+++ client/app/questionsIndex/questionsIndex.controller.js
@@ -1,9 +1,12 @@
 'use strict';
 
 angular.module('paizaqaApp')
-  .controller('QuestionsIndexCtrl', function ($scope, $http) {
+  .controller('QuestionsIndexCtrl', function ($scope, $http, Auth) {
     $scope.message = 'Hello';
     $http.get('/api/questions').success(function(questions) {
       $scope.questions = questions;
     });
+    $scope.isStar = function(obj){
+      return Auth.isLoggedIn() && obj && obj.stars && obj.stars.indexOf(Auth.getCurrentUser()._id)!==-1;
+    }
   });
